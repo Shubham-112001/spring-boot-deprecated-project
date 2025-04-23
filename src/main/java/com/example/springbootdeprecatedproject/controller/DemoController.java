3a04eecfd7c8bc0bb29eb4370ca8bfde797b6ac7
spadia <padiashubham0@gmail.com>
2025-04-23 15:03:33 +0530

    a

--- src/main/java/com/example/springbootdeprecatedproject/controller/DemoController.java
+++ src/main/java/com/example/springbootdeprecatedproject/controller/DemoController.java
@@ -1,3 +1,4 @@
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +19,7 @@ public class DemoController {
     @ResponseBody
     public String commonsExample() {
         // Example of using deprecated Apache Commons features
-        String result = org.apache.commons.lang.StringUtils.capitalise("example");
+        String result = StringUtils.replaceAll("Hello World!", "Hello World!", "Hello World!");
         return "Using deprecated Apache Commons: " + result;
     }
 }
