d6cbcdfc6f9d4a4028b3f4bc5b8e3f4a1cb66a66
spadia <padiashubham0@gmail.com>
2025-04-23 16:07:16 +0530

    a

--- src/main/java/com/example/springbootdeprecatedproject/controller/DemoController.java
+++ src/main/java/com/example/springbootdeprecatedproject/controller/DemoController.java
@@ -1,4 +1,8 @@
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.text.StrSubstitutor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +24,10 @@ public class DemoController {
     public String commonsExample() {
         // Example of using deprecated Apache Commons features
         String result = StringUtils.replaceAll("Hello World!", "Hello World!", "Hello World!");
+        RandomStringUtils.random(1);
+        ArrayUtils.isEquals(null, null);
+        ObjectUtils.hashCode(null);
+        ObjectUtils.hashCodeMulti(null);
         return "Using deprecated Apache Commons: " + result;
     }
 }
